5e8a037997cb74188ab282e08c198c41e3770b8d
Tom-Tarjama <35615575+Tom-Tarjama@users.noreply.github.com>
2018-04-30 13:33:21 -0400

    Add files via upload

---
+++ Pythagorean/PythagoreanTest.java
@@ -0,0 +1,7 @@
+public class PythagoreanTest{
+    public static void main(String[] args){
+        Pythagorean output = new Pythagorean();
+        double hypotenuseOutput = output.calculateHypotenuse(4, 6);
+        System.out.println("The hypotenuse is "+hypotenuseOutput);
+    }
+}
